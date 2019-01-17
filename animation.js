6263829299ad555bba3b4c7a5a2ab5313fa0d58b
Jack Cook <hello@jackcook.nyc>
2019-01-17 18:26:52 -0500

    Used cookie to make sure animation doesn't get annoying

--- animation.js
+++ animation.js
@@ -1,6 +1,51 @@
+function parseCookies() {
+  var cookiesList = document.cookie.split("; ");
+  var cookies = {};
+
+  for (var i = 0; i < cookiesList.length; i++) {
+    var cookieParts = cookiesList[i].split("=");
+    cookies[cookieParts[0]] = cookieParts[1];
+  }
+
+  return cookies;
+}
+
+function setAnimationCookie() {
+  var currentTime = new Date().getTime();
+  var eightHours = 1000 * 60 * 60 * 8;
+  var expireDate = new Date(currentTime + eightHours);
+
+  document.cookie = "animation=" + new Date() + "; expires=" + expireDate + "; path=/";
+}
+
+function shouldAnimate() {
+  var cookies = parseCookies();
+
+  if ("animation" in cookies) {
+    var lastAnimationTime = new Date(cookies["animation"]).getTime();
+    var timeSinceLastAnimation = new Date().getTime() - lastAnimationTime;
+    var eightHours = 1000 * 60 * 60 * 8;
+
+    return animation && timeSinceLastAnimation >= eightHours;
+  } else {
+    return animation;
+  }
+}
+
+var animationElement = document.getElementById("animation");
+var textElement = animationElement.getElementsByTagName("a")[0];
+
+if (!shouldAnimate()) {
+  animationElement.classList.add("done-fast");
+  document.getElementById("shield").classList.add("done-fast");
+}
+
 window.onload = function() {
-  var animationElement = document.getElementById("animation");
-  var textElement = animationElement.getElementsByTagName("span")[0];
+  if (!shouldAnimate()) {
+    return;
+  }
+
+  setAnimationCookie();
 
   var i = 0;
   var text = textElement.innerHTML;
